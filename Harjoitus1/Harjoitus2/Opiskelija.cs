cc0a9eb25dbd530302373cfdf9d993dbf1b1f2d5
Oskari Elomaa <Oskari.Elomaa@student.careeria.fi>
2023-03-31 14:13:18 +0300

    2nd update

---
+++ Harjoitus1/Harjoitus2/Opiskelija.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Harjoitus2
+{
+    internal class Opiskelija
+    {
+        public string Nimi { get; set; }
+        public string OpiskelijaID { get; set; }
+        public int Opintopisteet { get; set; }
+
+        public string TulostaData()
+        {
+            string OpiskelijaData = "Nimi: " + Nimi + "\nOpiskelija ID: " + OpiskelijaID + "\nOpintopisteet: " + Opintopisteet.ToString();
+
+            return OpiskelijaData;
+        }
+
+        public int MuokkaaOpintopisteitä()
+        {
+            Console.Write(Nimi + " opintopiste määrä on " + Opintopisteet.ToString() + "\nAnna opiskelijalle uusi opintopiste määrä: ");
+            Opintopisteet = int.Parse(Console.ReadLine());
+            return Opintopisteet;
+        }
+    }
+}
---
+++ Harjoitus1/Harjoitus2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Harjoitus2;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Opiskelija oppilas = new Opiskelija();
+
+        oppilas.Nimi = "Rekku Mordiin";
+        oppilas.OpiskelijaID = "123456A789B";
+        oppilas.Opintopisteet = 16;
+
+        Console.WriteLine(oppilas.TulostaData() + "\n");
+
+        Console.WriteLine(oppilas.MuokkaaOpintopisteitä() + " on " + oppilas.Nimi + " uusi oppintopiste määrä.");
+    }
+}
