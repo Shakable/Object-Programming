aefd4ceec475a302aab36a29906c17abeb610a0c
Oskari Elomaa <Oskari.Elomaa@student.careeria.fi>
2023-04-17 10:20:46 +0300

    Update 5 (Hopefully)

---
+++ Harjoitukset/Harjoitus5/Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus5
+{
+    internal class Number
+    {
+        public float Primary_Number { get; set; }
+        public float Secondary_Number { get;set; }
+
+        public static float Add(float PN, float SN)
+        {
+            return PN + SN;
+        }
+        public static float Subtract(float PN, float SN)
+        {
+            return PN - SN;
+        }
+        public static float Multiply(float PN, float SN) 
+        {  
+            return PN * SN; 
+        }
+        public static float Divide(float PN, float SN)
+        {
+            return PN / SN;
+        }
+    }
+}
---
+++ Harjoitukset/Harjoitus5/Program.cs
@@ -0,0 +1,17 @@
+﻿using Harjoitus5;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random Number_Random = new Random();
+        Number New_Number = new Number();
+
+        New_Number.Primary_Number = Number_Random.Next(100);
+        New_Number.Secondary_Number = Number_Random.Next(100);
+
+        Console.WriteLine(New_Number.Primary_Number + " + " + New_Number.Secondary_Number + " = " + Number.Add(New_Number.Primary_Number, New_Number.Secondary_Number));
+        Console.WriteLine(New_Number.Primary_Number + " - " + New_Number.Secondary_Number + " = " + Number.Subtract(New_Number.Primary_Number, New_Number.Secondary_Number));
+        Console.WriteLine(New_Number.Primary_Number + " * " + New_Number.Secondary_Number + " = " + Number.Multiply(New_Number.Primary_Number, New_Number.Secondary_Number));
+        Console.WriteLine(New_Number.Primary_Number + " / " + New_Number.Secondary_Number + " = " + Number.Divide(New_Number.Primary_Number, New_Number.Secondary_Number));
+    }
+}
