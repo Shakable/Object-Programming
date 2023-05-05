d321af56b7105d7b1c3af2623da0131148edf20e
Oskari Elomaa <oskari.elomaa@student.careeria.fi>
2023-05-05 14:13:53 +0300

    Update 7

--- Harjoitukset/Harjoitus12_KT/Program.cs
+++ Harjoitukset/Harjoitus12_KT/Program.cs
@@ -7,39 +7,10 @@ internal class Program
         List<Kortit> Kortteja = new List<Kortit>();
         List<int> arvotut = new List<int>();
 
-        Random rnd = new Random();
-
-        
-            
-        
-
-
         for (int i = 1; i <= 13; i++)
         {
-             int arvo = rnd.Next(1, 14);
-
             Kortit kortti = new Kortit(maa, i);
             Kortteja.Add(kortti);
-            
-            if (arvotut.Contains(arvo))
-            {
-
-                i--;
-
-
-            } else
-            {
-
-                arvotut.Add(arvo);
-
-            }
-
-            for (int j = 0; j <= 12; j++)
-            {
-
-                Console.WriteLine(Kortteja[arvotut[j]]);
-                
-            }
         }
 
         foreach (var kortti in Kortteja)
